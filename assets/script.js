4db39e7a792cf93866a877525ec0e2f1e7d757f7
Joseph Perry <josephperry720@gmail.com>
2020-12-17 12:39:17 -0500

    removed slide toggle

--- assets/script.js
+++ assets/script.js
@@ -4,21 +4,21 @@ $(document).ready(function () {
   var coll = document.getElementsByClassName("collapsible");
   var i;
 
-  // for (i = 0; i < coll.length; i++) {
-  //   coll[i].addEventListener("click", function () {
-  //     this.classList.toggle("active-col");
-  //     var content = this.nextElementSibling;
-  //     if (content.style.display === "block") {
-  //       content.style.display = "none";
-  //     } else {
-  //       content.style.display = "block";
-  //     }
-  //   });
-  // }
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active-col");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
 
-  $(".collapsible").on("click", function() {
-    $(this).next().slideToggle();
-  })
+  // $(".collapsible").on("click", function() {
+  //   $(this).next().slideToggle();
+  // })
 
   // ICONS AND BUTTON VARIABLES
   var randomButton = $("#user-select-random");
